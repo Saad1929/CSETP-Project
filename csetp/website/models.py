3452b4458c932f3d0307cea1cf2650a5afcca7ef
Saad Touqeer Baig <2520863b@student.gla.ac.uk>
2025-02-05 22:28:02 +0000

    Employee Database Model

--- csetp/website/models.py
+++ csetp/website/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Employee(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    job_title = models.CharField(max_length=100)
+    department = models.CharField(max_length=100)
+    salary = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} - {self.job_title}"
+
