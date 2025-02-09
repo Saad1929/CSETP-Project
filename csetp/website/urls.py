985e32b55e03eeb03f8cc3fbe20b8f6ac3e70173
KevinCahill9 <2685130c@student.gla.ac.uk>
2025-02-09 17:29:22 +0000

    added database page, displays empoloyees

--- csetp/website/urls.py
+++ csetp/website/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('primm/', views.primm, name="primm")
+    path('primm/', views.primm, name="primm"),
+    path('database/', views.database_employee, name="employee-list"),
 ]
--- csetp/website/views.py
+++ csetp/website/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from .models import Employee
 
 def home(request):
     return render(request, "home.html", {})
 
 def primm(request):
     return render(request, "primm.html", {})
+
+def database_employee(request):
+    employees = Employee.objects.all()
+    return render(request, "database.html", {'employees': employees})
